a216237e39b03df20fc6ce2efede156575d257b2
Reza Mesgaran <reza_mesgaran@live.com>
2019-06-05 23:50:57 -0400

    Calculate Factorial by Recursive Method

--- Console_RecursiveMethod/Program.cs
+++ Console_RecursiveMethod/Program.cs
@@ -10,6 +10,18 @@ namespace Console_RecursiveMethod
     {
         static void Main(string[] args)
         {
+            Console.Write("Enter a number to calculate it's factorial:");
+            double n=Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("factorial of {0} is {1}",n, Fact(n));
+            Console.ReadKey();
+        }
+
+        public static double Fact(double x)
+        {
+            if (x == 1)
+                return 1;
+            else
+                return x * Fact(x - 1);            
         }
     }
 }
